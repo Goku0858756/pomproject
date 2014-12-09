fc75bbcbee0a78a5e27c1c02456c91de50dff410
Goku0858756 <gokhan_kacan@hotmail.com>
2014-12-09 12:01:36 +0100

    Dit toevoegen aan mijn gitProject

---
+++ mavenTest/src/main/java/com/gokhankacan/mavenTest/App.java
@@ -0,0 +1,18 @@
+package com.gokhankacan.mavenTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public boolean funny(boolean fun) {
+    	fun = true;
+    	return fun;
+    }
+}
